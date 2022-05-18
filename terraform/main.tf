4755ac1c4e084435d8f8df55e2da30e6b5739c52
JGustavo11 <jgustavoht@gmail.com>
2022-05-18 14:00:41 -0700

    refactor(main,var.tf): Adds blank resources with some values

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,68 @@
-// Here is where you should configure your remote state
+#Here you will configure your terraform and backend 
 terraform {
-  backend {}
-
-  required_providers {
-    aws = {
-      souce = "hashicorp/aws"
-    }
+  backend "s3" {
+   
   }
 }
 
-// We will be using AWS for this challenge
+# Here you will configure your provider (AWS)
 provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
+  
+
+}
+
+#Gets the default vpc
+data "aws_vpc" "default" {
+default = true
+}
+
+#Gets the defaults VPC's security group
+data "aws_security_group" "defaultvpc" {
+  vpc_id = data.aws_vpc.default.id
+
+  filter {
+    values = ["default"]
+  }
+}
+#Queries zone ID
+data "aws_route53_zone" "selected" {
+  name         = "bwtcdevopschallenge.com."
+}
+
+#Here you will configure your security group to allow SSH on port 22 from ONLY your IP address.
+resource "aws_security_group" "allow_ssh" {
+ 
+
+}
+
+#Here you will provide your keypair. Currently this resource requires an existing user-supplied key pair. This key pair's public key will be registered with AWS to allow logging-in to EC2 instances.
+#Read about it here https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
+resource "aws_key_pair" "deployer" {
+  key_name   = "Name of Key "
+  public_key = "ssh-rsa key"
+}
+
+# #Here You will be creating the EC2 server. 
+resource "aws_instance" "web" {
+  vpc_security_group_ids = [
+      "${data.aws_security_group.defaultvpc.ids}",
+  ]
+
+  root_block_device {
+    #Fill out the volume size and storage in here.
+  }
+}
+
+
+#Create S3 
+resource "aws_s3_bucket" "bucket_creation" {
+
+
+}
+
+#Create route 53 A record to point to IP of EC2
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+
 }
 
